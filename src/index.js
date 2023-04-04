e54291dba95e7a333a0febf355de5803258aa451
KleemoffDeveloper <kleemoffdeveloper@gmail.com>
2023-04-04 09:13:04 -0400

    adjusted styling and changed searchbar

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const BASE_URL = 'https://rickandmortyapi.com/api/character';
 
 let allCharacters = [];
 let cards;
+let indexCounter = 0;
 
 /**
  * 
@@ -11,16 +12,18 @@ function start(url) {
     fetch(url)
     .then(response => response.json())
     .then(data => {
-        data.results.forEach(character => {
+        data.results.forEach((character) => {
+            indexCounter++;
+            createCard(character, indexCounter);
+
+            // Random compatibility (satire)
             character.compatible = Math.floor(Math.random() * 2) ? true : false;
-            allCharacters.push(character)
+
+            allCharacters.push(character);
         });
 
         if(data.info.next) {
             start(data.info.next);
-        }
-        else {
-            createCards(allCharacters.length);
         }
     })
     .catch(error => console.log(error));
@@ -28,39 +31,36 @@ function start(url) {
 
 /**
  * 
- * @param {number} amount - The amount of cards that get generated.
+ * @param {object} cardObject - An object that represents a character from the rickandmortyapi JSON.
+ * @param {number} index - The index (count) of the character from the API.
  */
-function createCards(amount) {
-    console.log(allCharacters[600]);
+function createCard(cardObject, index) {
+    let card = document.createElement('div');
+    card.className = 'box-style-1';
 
-    for(let i = 0; i < amount; i++) {
-        let card = document.createElement('div');
-        card.className = 'box-style-1';
+    // Upper box
+    let h1 = document.createElement('h1');
+    h1.innerText = (index) + '. ' + cardObject.name;
 
-        // Upper box
-        let h1 = document.createElement('h1');
-        h1.innerText = (i + 1) + '. ' + allCharacters[i].name;
+    let hr = document.createElement('hr');
 
-        let hr = document.createElement('hr');
+    let img = document.createElement('img');
+    img.src = cardObject.image;
 
-        let img = document.createElement('img');
-        img.src = allCharacters[i].image;
+    // Lower box
+    let lower = document.createElement('div');
+    lower.className = 'lower-box';
 
-        // Lower box
-        let lower = document.createElement('div');
-        lower.className = 'lower-box';
+    let p = document.createElement('p');
 
-        let p = document.createElement('p');
+    p.innerText = `Gender: ${cardObject.gender}\nFrom: ${cardObject.origin.name}\nSpecies: ${cardObject.species}`;
 
-        p.innerText = `Gender: ${allCharacters[i].gender}\nFrom: ${allCharacters[i].origin.name}\nSpecies: ${allCharacters[i].species}`
+    card.append(h1, hr, img);
+    lower.append(p);
+    
+    card.append(lower);
 
-        card.append(h1, hr, img);
-        lower.append(p);
-        
-        card.append(lower);
-
-        document.querySelector('.all-characters').append(card);
-    }
+    document.querySelector('.all-characters').append(card);
 
     cards = document.querySelectorAll('.box-style-1');
 }
@@ -72,12 +72,25 @@ let searchForm = document.querySelector('.searchbar');
 searchForm.onsubmit = (event) => {
     event.preventDefault();
 
+    let searchQuery = searchForm.querySelector('input').value;
+
     for(let i = 0; i < allCharacters.length; i++) {
-        if(!allCharacters[i].compatible) {
-            console.log(cards[i]);
-            cards[i].style.display = "none";
-        } else {
-            cards[i].querySelector('h1').innerHTML += " (<style=color:green;>COMPATIBLE</style>)";
+        let data = allCharacters[i];
+        let card = cards[i];
+
+        let show = 
+            (data.name.toLowerCase().includes(searchQuery) 
+        || data.gender.toLowerCase().includes(searchQuery) 
+        || data.species.toLowerCase().includes(searchQuery)
+        || data.origin.name.toLowerCase().includes(searchQuery)) && searchQuery !== '';
+
+        if(show) {
+            card.style.display = "";
+
+            card.style.background = "linear-gradient(to bottom, yellowgreen, green)"
+        }
+        else {
+            card.style.display = "none";
         }
     }
 }
